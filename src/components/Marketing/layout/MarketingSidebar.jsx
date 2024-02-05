14df6d38c0f270677cded55be0f2a821f2e26726
gozzafadillah <gozzafadillah@gmail.com>
2024-02-05 07:52:40 +0700

    fix/sidebar-marketing

--- src/components/Marketing/layout/MarketingSidebar.jsx
+++ src/components/Marketing/layout/MarketingSidebar.jsx
@@ -13,21 +13,6 @@ const MarketingSidebar = ({ active }) => {
   return (
     <div className="w-full h-[90vh] bg-white shadow-sm overflow-y-scroll sticky top-0 left-0 z-10">
       {/* single item */}
-      <div className="w-full flex items-center p-4">
-        <Link to="/admin/dashboard" className="w-full flex items-center">
-          <RxDashboard
-            size={30}
-            color={`${active === 1 ? "crimson" : "#555"}`}
-          />
-          <h5
-            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
-              active === 1 ? "text-[crimson]" : "text-[#555]"
-            }`}
-          >
-            Dashboard
-          </h5>
-        </Link>
-      </div>
 
       <div className="w-full flex items-center p-4">
         <Link to="/data-kriteria" className="w-full flex items-center">
