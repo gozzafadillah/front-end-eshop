85ccd248ec7228661f585ada561f5496506f546a
gozzafadillah <gozzafadillah@gmail.com>
2024-02-05 07:00:42 +0700

    fieat/marketing

--- src/server.js
+++ src/server.js
@@ -1,2 +1,2 @@
 // export const server = "https://eshop-tutorial.vercel.app/api/v2";
-export const server = process.env.REACT_APP_API_URL;
+export const server = "https://backendeshop-production.up.railway.app/api/v2";
